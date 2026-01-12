4483bb88ae6338a5af6343af83f60c4d205e8aa6
AlexBraun-NAIT <alexbraunreal1@gmail.com>
2026-01-12 14:21:03 -0700

    End of First Day

---
+++ GMPR2512 Universal 2D/Assets/GMPR2512/Lesson 1/Square.cs
@@ -0,0 +1,23 @@
+using System.Diagnostics;
+using UnityEngine;
+
+namespace GMPR2512.Lesson01
+{
+    public class Square : MonoBehaviour
+    {
+        // Start is called once before the first execution of Update after the MonoBehaviour is created
+        void Start()
+        {
+        Debug.log("hello world");
+        {
+            
+        }
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        
+        }
+    }
+}
